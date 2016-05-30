94fe82111719155d71ec24d2b959cd30f2b5707d
Thibaud Colas <thibaudcolas@gmail.com>
2016-05-30 17:33:13 +0300

    Add more details to first reducers' comments

--- reducers/exercise0.js
+++ reducers/exercise0.js
@@ -1,30 +1,38 @@
-// Here is your reducer 
-// It's "listening" for some actions to happen once a dispatch occurred.
+// Here is one of the reducers.
+// A reducer is a function that receives actions and changes the store state accordingly.
+// It's "listening" for some actions to happen once a `dispatch` occurred.
 // In this case this reducer is listening for "CHANGE_VALUE" action.
 // But it could listen for many more actions.
-// It just assigns/overwrites the new value to its state object in the app store
-// reducers ALWAYS return the state (new or unchanged)
-// The app store (or kind of root state if you prefer) contains the state of each reducers.
-// For instance you will access to this one using 'state.exercise0.whatevervalue'
+
+// When an action occuers, it changes the store's state to reflect what that action conveys.
+// The store state needs to be immutable: a new state is re-created from the old one, hence why we use `assign`.
+// Reducers ALWAYS return the state (new or unchanged).
+
+// An application can be composed of multiple reducers, each of them acting on a separate part of the overall state.
+// The global application state within the Redux store can be created by
+// combining the sub-states retuned by all of the reducers.
+// Here, this reducer works on the "exercise0" part of the state, available
+// in `state.exercise0.whatevervalue`
 // Get started on Exercise 1 once you've understood this file :)
 
 import { CHANGE_VALUE } from '../actions/';
 
+// Each reducer must define the initial state it works on.
 const initialState = {
-  value: 'Initial value',
+    value: 'Initial value',
 };
 
-const exercise = (state = initialState, action) => {
-  // usually reducer core is just a switch on action.type
-  // if you need to perform operations on values, create an external function and use it
-  switch (action.type) {
+const exercise0 = (state = initialState, action) => {
+    // usually reducer core is just a switch on action.type
+    // if you need to perform operations on values, create an external function and use it
+    switch (action.type) {
     case CHANGE_VALUE:
-      return Object.assign({}, state, {
-        value: action.newValue,
-      })
+        return Object.assign({}, state, {
+            value: action.newValue,
+        });
     default:
-      return state
-  }
-}
+        return state;
+    }
+};
 
-export default exercise
+export default exercise0;
